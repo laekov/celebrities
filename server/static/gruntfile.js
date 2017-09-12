6c7414bffdd38cd39e3f2f5fb64ec805e7bf3808
laekov <laekov.h@gmail.com>
2017-09-12 21:03:38 +0800

    angularFrame

--- server/static/gruntfile.js
+++ server/static/gruntfile.js
@@ -1,11 +1,17 @@
+var angularFiles = [ 
+	'bower_components/angular/angular.min.js', 
+	'bower_components/angular-ui-router/release/angular-ui-router.min.js',
+	'bower_components/oclazyload/dist/ocLazyLoad.min.js',
+];
 module.exports = function(grunt) {
     grunt.initConfig({
 		concat: {
 			dev: {
 				files: {
-					//'dists/router.js': [ 'router/*' ],
+					'dists/router.js': [ 'router/*' ],
 					'dists/controller.js': [ 'modules/*/*.js' ],
-				}
+					'dists/lib.angular.js': angularFiles
+				},
 			}
 		},
     });
